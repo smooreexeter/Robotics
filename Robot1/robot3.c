c077797740c4a7259cc9588fe710521cea4e75da
smooreexeter <smoore@exeter.edu>
2016-04-04 17:56:20 -0400

    Update robot3.c

--- Robot1/robot3.c
+++ Robot1/robot3.c
@@ -5,6 +5,7 @@
 
 //graph of distance vs times can be found at https://www.desmos.com/calculator/oki7aqrys9
 
+
 void wait(int len){
 	wait1Msec(len);
 }
@@ -30,15 +31,23 @@ void rampUp(int length, int targetSpeed, float fac, int dir){
 }
 
 void move(int length, int dir){
+	
+		
+	int speedl = 58;
+	int speedr = 48;
+
 
 	//dir controls direction. Positive value goes forward, negative value goes backwards.
 	//k is some constant which relates distance traveled to time in units of s/cm
+	
+	length = length * 1.338 - 8.6984;
+	
 	length -= 9;
 	float k = 20.12; //21.739
 	float b = 365.78;
 	int time = length * k + b;
-	int speedl = sgn((float) dir) * 58;
-	int speedr = sgn((float) dir) * 46;
+	speedl = sgn((float) dir) * speedl;
+	speedr = sgn((float) dir) * speedr;
 	
 	rampUp(0,speedl, 0.7931, dir);
 	
@@ -53,16 +62,21 @@ void move(int length, int dir){
 }
 
 void turn90(int dir, int mult, bool waitVar){
+		
+		
+	int speedl = 58;
+	int speedr = 48;
+	
 
 	for(int i = 0; i < mult; i++){
 		//change time depending on how long it actually takes to make a right turn;
 		//positive dir makes a right turn, negative makes a left turn;
-		int time = 800;
-		int speedl = 58;//sgn((float) dir) * 58;
-		int speedr = 46;//sgn((float) dir) * 46;
+		int time = 1600;
+		//speedl = .9 * speedl;//sgn((float) dir) * 58;
 		
-		float tr = 1.5;
-		float tl = 0.9;
+		
+		float tr = 0.8;
+		float tl = 0.65;
 		
 		if(dir > 0){
 			motor[leftMotor] = speedl;
@@ -94,11 +108,16 @@ task main(){
 	//move(200, 1);
 	//move(150, 1);
 	///*
-	move(200,1);
+	move(200, 1);
+	//wait(20000);
 	turn90(-1, 1, true);
 	move(250,1);
+	//move(100, 1);
+	//wait(20000);
 	turn90(1, 2, true);
 	move(56, 1);
+	//move(50, 1);
+	//wait(20000);
 	turn90(1, 3, true);
 	move(300, 1);
 	turn90(-1, 12, false);
